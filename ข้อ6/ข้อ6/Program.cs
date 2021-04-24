e3eaec4036a8570ca10c136c5a4c54c0d8c25553
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-04-24 15:59:38 +0700

    ข้อ6

---
+++ ข้อ6/ข้อ6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+
+            int W = int.Parse(Console.ReadLine());
+            int H = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+
+            for ( int i = 1; i <= H*N; i++)
+            {
+            
+                for (int j = 1; j <= H; j++)
+                {
+                    Console.Write(input);
+                }
+                
+                Console.WriteLine(input);
+
+                
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
