0e5c345d43db1d6f0db06713de0b22f158246b61
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-04-24 15:59:26 +0700

    ข้อ5

---
+++ ข้อ5/ข้อ5/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DayofWeek = int.Parse(Console.ReadLine());
+            int TimeofDay = int.Parse(Console.ReadLine());
+
+            string Menu = Console.ReadLine();
+
+            bool breakfast = false;
+            bool weekend = false;
+            bool coffee = false;
+
+
+            Console.ReadLine();
+
+            do
+            {
+                string Menu1;
+                Menu1 = Menu;
+                for (int i = 1; i <= 5; i++)
+                {
+                    
+                    if (Menu1 == "Breakfast Set") ;
+                    {
+                        breakfast = true;
+                    }
+
+                }
+
+                for (int i = 1; i <= 2; i++)
+                {
+                    
+                    if (Menu1 == "Weekend Set") ;
+                    {
+                        weekend = true;
+                    }
+
+                }
+                for (int i = 1; i <= 3; i++)
+                {
+                    
+                    if (Menu1 == "Coffee") ;
+                    {
+                        coffee = true;
+                    }
+                }
+            } while (Menu == "End");
+
+        }
+
+       
+    }
+}
