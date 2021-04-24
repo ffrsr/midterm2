e1391a0a6cb8783c79f5a3b9bbbf941d7c557037
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-04-24 15:59:55 +0700

    ข้อ7

---
+++ ข้อ7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());  
+            int b = int.Parse(Console.ReadLine());
+
+            char input = char.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+
+            if (input == '+' || input == '-' || input == '*' || input == '/')
+            {
+                int c = int.Parse(Console.ReadLine());
+                int d = int.Parse(Console.ReadLine());
+            }
+
+            else if (input == '^')
+            {
+                int n = int.Parse(Console.ReadLine());
+            }
+
+            else if (input == '|')
+            { }
+
+            else { }
+            
+        }
+    }
+}
